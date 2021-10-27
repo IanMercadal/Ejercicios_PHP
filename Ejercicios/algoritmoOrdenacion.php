e86810fd628811e8adaa888587d1bad04b2ea946
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2021-10-27 16:50:55 +0200

    feat: :art: Algoritmo comentado y finalizado
    Comentarios y código finalizado

--- Ejercicios/algoritmoOrdenacion.php
+++ Ejercicios/algoritmoOrdenacion.php
@@ -1,7 +1,13 @@
 <?php
     function ordenar(){
+
+        // Creamos un array en el que vamos a almacenar los valores insertados por pantalla
+
         $array = [];
+
         echo "<h2>Introduce 5 números</h2> \n";
+
+        // Los números pedidos por pantalla se almacenarán en su respectiva variable
 
         $primero = (int)readline('Introduce el primer integer: ');
         $segundo = (int)readline('Introduce el segundo integer: ');
@@ -12,16 +18,24 @@
         array_push($array,$primero,$segundo, $tercero,$cuarto,$quinto); // Insertamos los valores en el array
 
         $longitud = count($array); // Medimos la longitud del array
+
+        // Mejorar redacción de la lógica
             
         for($i = 0; $i< $longitud; $i++){ // Creamos la variable i y debe de ser menor que el array
-            for($j = 0; $j < $longitud -1; $j++){ // Cramos la variable j 
-                if($array[$j] > $array[$j+1]){  // Si j número es mayor a j +1(número de la derecha)
-                    $k = $array[$j]; // Creamos k, como número a comparar por cada iteración(este va variando), que vamos a comparar con el número de la derecha
-                    $array[$j] = $array[$j+1]; // Realizamos un intercambio de valores si es mayor
+            for($j = 0; $j < $longitud -1; $j++){ // Cramos la variable j, que nos permite ver el numero de iteraciones
+                if($array[$j] > $array[$j+1]){  // Si j número es mayor a j +1(número de la derecha) entra y realiza esto:
+
+                    // Creamos k, como número j con su respectivo valor en el array.
+                    $k = $array[$j];
+
+                    // Realizamos un intercambio de valores, en el que j pasa a tener el valor del número de la derecha del que estamos comparando.
+                    $array[$j] = $array[$j+1]; 
+                    
+                    // En esta parte, k pasaría a estar en la posición del número de su derecha al ser mayor
                     $array[$j +1] = $k;
                 }
             }  
         }
-        print_r($array);
+        var_dump($array);
     }
 ordenar();
